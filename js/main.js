05cfcca99a419049fbff7e6b49721a68796fba15
Sinmiloluwa <mofeoluwae@gmail.com>
2021-09-25 09:47:13 -0700

    Fetch Api implemented

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-// data to be sent to the POST request
-let _data = {
-  amount: 1000,
+
+function calculate() {
+	var input = document.getElementById('amount').value;
+	// data to be sent to the POST request
+	let _data = {
+  	amount: input,
+	}
+
+	fetch('http://pennee.herokuapp.com/api/result', {
+	  method: "POST",
+	  mode: 'cors',
+	  body: JSON.stringify(_data),
+	  headers: {"Content-type": "application/json; charset=UTF-8",
+	  			"Access-Control-Allow-Origin" :"http://pennee.herokuapp.com"
+	}
+	})
+	.then(response => response.json()) 
+	.then(json => {
+		console.log(json.data)
+		document.getElementById('price').innerText = json.data.toFixed(2)
+	})
+	.catch(err => console.log(err));
+
 }
 
-fetch('https://pennee.herokuapp.com/api/result', {
-  method: "POST",
-  mode: 'cors',
-  body: JSON.stringify(_data),
-  headers: {"Content-type": "application/json; charset=UTF-8",
-  			"Access-Control-Allow-Origin" :"http://pennee.herokuapp.com"
-}
-})
-.then(response => response.json()) 
-.then(json => console.log(json))
-.catch(err => console.log(err));
+
+
+
